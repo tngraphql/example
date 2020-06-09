b050c074922bae8225b9cd58391f32398dd280f7
nguyenpl117 <nguyenpl117@gmail.com>
2020-06-09 13:05:04 +0700

    ...

---
+++ src/app/Models/NotificationModel.ts
@@ -0,0 +1,16 @@
+import { DateTime } from 'luxon'
+import { column } from '@tngraphql/lucid/build/src/Orm/Decorators';
+import { BaseModel } from '@tngraphql/lucid/build/src/Orm/BaseModel';
+
+export default class NotificationModel extends BaseModel {
+    @column({ isPrimary: true })
+    public id: number
+
+    @column.dateTime({ autoCreate: true })
+    public createdAt: DateTime
+
+    @column.dateTime({ autoCreate: true, autoUpdate: true })
+    public updatedAt: DateTime
+
+    public static $columns: Pick<NotificationModel, 'id' | 'createdAt' | 'updatedAt'>
+}
---
+++ src/database/seeds/UserSeed.ts
@@ -0,0 +1,19 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| UserSeed
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+import {Factory} from "@tngraphql/illuminate/dist/Support/Facades";
+
+export class UserSeed {
+    public async run() {
+        await Factory.model('App/UserModel').create();
+    }
+}
