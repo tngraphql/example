6a162a283a0a729086a6afcb6ef2175ba6133247
nguyenpl117 <nguyenpl117@gmail.com>
2020-08-30 14:03:45 +0700

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,10 @@ async function main() {
 
     express.use(media);
 
-    await express.listen(4002, '127.0.0.1');
+    const port = process.env.PORT || 3000
+    const host = process.env.HOST || '127.0.0.1'
+
+    await express.listen(port, host);
     console.timeEnd();
 }
 
